2f25c781ff96394bcc1b9c11bbbc15de5676e23c
Firestone82 <mikpav@email.cz>
2024-02-21 16:50:48 +0100

    Finished Bonus JS

--- task-bonus.js
+++ task-bonus.js
@@ -8,8 +8,15 @@
 
 // Your code:
 export const drawTriangle = (length = 5) => {
-  
-    // ... write code ...
+  for (let i = 1; i <= length; i = i + 1) {
+    let line = "";
+
+    for (let j = 1; j <= i; j = j + 1) {
+      line = line + "* ";
+    }
+
+    console.log(line);
+  }
 };
 
 // 2#  ========== BONUS =======================
@@ -28,18 +35,31 @@ export const drawTriangle = (length = 5) => {
 
 // Your code:
 export const drawJavascriptWord = (word = "javascript") => {
-  // ... write code ...
-};
+  for (let i = 0; i < word.length; i++) {
+    let line = "";
 
+    for (let j = 0; j < word.length; j++) {
+      if (j > i) {
+        line = line + "* ";
+      } else {
+        line = line + word[word.length - j - 1].toUpperCase() + " ";
+      }
+    }
+
+    // Reverse string
+    line = line.split(" ").reverse().join(" ").trim();
+    console.log(line);
+  }
+};
 
 // 3#  ========== BONUS =======================
 // Create function that takes array of vehicles with measured top speeds. Return array of vehicle with top speed.
 // Example:
-// const vehicles = [
-//   { name: "Executor Star Dreadnought", measuredSpeeds: [555, 545, 577, 600] },
-//   { name: "T-47 Airspeeder", measuredSpeeds: [300, 311, 299, 350] },
-//   { name: "AT-AT", measuredSpeeds: [20, 21, 20, 19] },
-// ];
+const vehicles = [
+  { name: "Executor Star Dreadnought", measuredSpeeds: [555, 545, 577, 600] },
+  { name: "T-47 Airspeeder", measuredSpeeds: [300, 311, 299, 350] },
+  { name: "AT-AT", measuredSpeeds: [20, 21, 20, 19] },
+];
 // getVehiclesAndTopSpeed(vehicles) ➞ will return ➞ [
 //     { name: "Executor Star Dreadnought", topSpeed: 600},
 //     { name: "T-47 Airspeeder", topSpeed: 350 },
@@ -48,5 +68,14 @@ export const drawJavascriptWord = (word = "javascript") => {
 
 // Your code:
 export const getVehiclesAndTopSpeed = (vehicles) => {
-  
+  return vehicles.map((vehicle) => {
+    return {
+      name: vehicle.name,
+      topSpeed: Math.max(...vehicle.measuredSpeeds),
+    };
+  });
 };
+
+drawTriangle();
+drawJavascriptWord();
+console.log(getVehiclesAndTopSpeed(vehicles));
